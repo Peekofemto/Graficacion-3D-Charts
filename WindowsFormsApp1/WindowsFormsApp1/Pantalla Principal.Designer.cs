629d51da67ef1bebe4ad0d85ece856768f335bd5
Peeko <omarock27@gmail.com>
2018-05-16 19:24:55 -0700

    Casi terminado
    Programa casi terminado

---
+++ WindowsFormsApp1/WindowsFormsApp1/Pantalla Principal.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lenguajes2010BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.lenguajesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.lenguajesDataSet = new WindowsFormsApp1.LenguajesDataSet();
+            this.lenguajesTableAdapter = new WindowsFormsApp1.LenguajesDataSetTableAdapters.LenguajesTableAdapter();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cbEstiloGraf = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnBackColor = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnColorSerie = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbIluminacion = new System.Windows.Forms.ComboBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.cbSeries = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnTodas = new System.Windows.Forms.Button();
+            this.btnSerie2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnSerie3 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lenguajes2010BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lenguajesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lenguajesDataSet)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.DataSource = this.lenguajesBindingSource;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series1.IsValueShownAsLabel = true;
+            series1.Legend = "Legend1";
+            series1.Name = "2018";
+            series1.XValueMember = "Lenguaje";
+            series1.YValueMembers = "Popularidad";
+            series1.YValuesPerPoint = 4;
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "2010";
+            series2.XValueMember = "Lenguaje";
+            series2.YValueMembers = "Popularidad2010";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "2000";
+            series3.XValueMember = "Lenguaje";
+            series3.YValueMembers = "Popularidad2000";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(502, 461);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // lenguajes2010BindingSource
+            // 
+            this.lenguajes2010BindingSource.DataMember = "Lenguajes2010";
+            // 
+            // lenguajesBindingSource
+            // 
+            this.lenguajesBindingSource.DataMember = "Lenguajes";
+            this.lenguajesBindingSource.DataSource = this.lenguajesDataSet;
+            // 
+            // lenguajesDataSet
+            // 
+            this.lenguajesDataSet.DataSetName = "LenguajesDataSet";
+            this.lenguajesDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // lenguajesTableAdapter
+            // 
+            this.lenguajesTableAdapter.ClearBeforeFill = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2D",
+            "3D"});
+            this.comboBox1.Location = new System.Drawing.Point(657, 42);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // cbEstiloGraf
+            // 
+            this.cbEstiloGraf.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbEstiloGraf.FormattingEnabled = true;
+            this.cbEstiloGraf.Items.AddRange(new object[] {
+            "Columna",
+            "Barra",
+            "Pie",
+            "Linea ",
+            "Area"});
+            this.cbEstiloGraf.Location = new System.Drawing.Point(657, 76);
+            this.cbEstiloGraf.Name = "cbEstiloGraf";
+            this.cbEstiloGraf.Size = new System.Drawing.Size(121, 21);
+            this.cbEstiloGraf.TabIndex = 2;
+            this.cbEstiloGraf.SelectedIndexChanged += new System.EventHandler(this.cbEstiloGraf_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(586, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tipo Grafico";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(586, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Estilo Grafico";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSerie3);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.btnSerie2);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.btnTodas);
+            this.groupBox1.Controls.Add(this.btnBackColor);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.btnColorSerie);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cbIluminacion);
+            this.groupBox1.Location = new System.Drawing.Point(538, 144);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(328, 343);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Extra";
+            // 
+            // btnBackColor
+            // 
+            this.btnBackColor.Location = new System.Drawing.Point(173, 183);
+            this.btnBackColor.Name = "btnBackColor";
+            this.btnBackColor.Size = new System.Drawing.Size(121, 23);
+            this.btnBackColor.TabIndex = 11;
+            this.btnBackColor.Text = "Color";
+            this.btnBackColor.UseVisualStyleBackColor = true;
+            this.btnBackColor.Click += new System.EventHandler(this.btnBackColor_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(79, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Color de fondo";
+            // 
+            // btnColorSerie
+            // 
+            this.btnColorSerie.Location = new System.Drawing.Point(173, 42);
+            this.btnColorSerie.Name = "btnColorSerie";
+            this.btnColorSerie.Size = new System.Drawing.Size(121, 23);
+            this.btnColorSerie.TabIndex = 9;
+            this.btnColorSerie.Text = "Color";
+            this.btnColorSerie.UseVisualStyleBackColor = true;
+            this.btnColorSerie.Click += new System.EventHandler(this.btnColorSerie_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(79, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Estilo Ilimunación";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Color Serie 1";
+            // 
+            // cbIluminacion
+            // 
+            this.cbIluminacion.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbIluminacion.FormattingEnabled = true;
+            this.cbIluminacion.Items.AddRange(new object[] {
+            "Ninguno",
+            "Simplista",
+            "Realista"});
+            this.cbIluminacion.Location = new System.Drawing.Point(173, 145);
+            this.cbIluminacion.Name = "cbIluminacion";
+            this.cbIluminacion.Size = new System.Drawing.Size(121, 21);
+            this.cbIluminacion.TabIndex = 6;
+            this.cbIluminacion.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // cbSeries
+            // 
+            this.cbSeries.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSeries.FormattingEnabled = true;
+            this.cbSeries.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.cbSeries.Location = new System.Drawing.Point(657, 113);
+            this.cbSeries.Name = "cbSeries";
+            this.cbSeries.Size = new System.Drawing.Size(121, 21);
+            this.cbSeries.TabIndex = 6;
+            this.cbSeries.SelectedIndexChanged += new System.EventHandler(this.cbSeries_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(589, 113);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Series";
+            // 
+            // btnTodas
+            // 
+            this.btnTodas.Location = new System.Drawing.Point(82, 224);
+            this.btnTodas.Name = "btnTodas";
+            this.btnTodas.Size = new System.Drawing.Size(212, 23);
+            this.btnTodas.TabIndex = 12;
+            this.btnTodas.Text = "Mostrar todas las series";
+            this.btnTodas.UseVisualStyleBackColor = true;
+            this.btnTodas.Click += new System.EventHandler(this.btnTodas_Click);
+            // 
+            // btnSerie2
+            // 
+            this.btnSerie2.Location = new System.Drawing.Point(173, 71);
+            this.btnSerie2.Name = "btnSerie2";
+            this.btnSerie2.Size = new System.Drawing.Size(121, 23);
+            this.btnSerie2.TabIndex = 14;
+            this.btnSerie2.Text = "Color";
+            this.btnSerie2.UseVisualStyleBackColor = true;
+            this.btnSerie2.Click += new System.EventHandler(this.btnSerie2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(79, 81);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Color Serie 2";
+            // 
+            // btnSerie3
+            // 
+            this.btnSerie3.Location = new System.Drawing.Point(173, 106);
+            this.btnSerie3.Name = "btnSerie3";
+            this.btnSerie3.Size = new System.Drawing.Size(121, 23);
+            this.btnSerie3.TabIndex = 16;
+            this.btnSerie3.Text = "Color";
+            this.btnSerie3.UseVisualStyleBackColor = true;
+            this.btnSerie3.Click += new System.EventHandler(this.btnSerie3_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(79, 111);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Color Serie 3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 499);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cbSeries);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbEstiloGraf);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "kk";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lenguajes2010BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lenguajesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lenguajesDataSet)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private LenguajesDataSet lenguajesDataSet;
+        private System.Windows.Forms.BindingSource lenguajesBindingSource;
+        private LenguajesDataSetTableAdapters.LenguajesTableAdapter lenguajesTableAdapter;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbEstiloGraf;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbIluminacion;
+        private System.Windows.Forms.Button btnColorSerie;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btnBackColor;
+        private System.Windows.Forms.Label label5;
+
+        private System.Windows.Forms.BindingSource lenguajes2010BindingSource;
+        private System.Windows.Forms.ComboBox cbSeries;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnTodas;
+        private System.Windows.Forms.Button btnSerie3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnSerie2;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Pantalla Principal.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.lenguajesTableAdapter.Fill(this.lenguajesDataSet.Lenguajes);
+            comboBox1.SelectedIndex = 0;
+            chart1.Series[0].Enabled = true;
+            chart1.Series[1].Enabled = false;
+            chart1.Series[2].Enabled = false;
+
+        }
+
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            chart1.ChartAreas.Select(x => { x.Area3DStyle.Enable3D = comboBox1.SelectedIndex.Equals(1); return x; }).First();
+            //if (comboBox1.SelectedIndex == 0)
+            //{
+            //    chart1.ChartAreas[0].Area3DStyle.Enable3D = false;
+            //    chart1.ChartAreas[1].Area3DStyle.Enable3D = false;
+            //    chart1.ChartAreas[2].Area3DStyle.Enable3D = false;
+            //}
+            //else
+            //{
+            //    chart1.ChartAreas[0].Area3DStyle.Enable3D = true;
+            //    chart1.ChartAreas[1].Area3DStyle.Enable3D = true;
+            //    chart1.ChartAreas[2].Area3DStyle.Enable3D = true;
+            //}
+
+        }
+        ChartArea Area2 = new ChartArea("Area2");
+
+        ChartArea Area3 = new ChartArea("Area3");
+        
+
+
+        private void cbEstiloGraf_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cbEstiloGraf.SelectedIndex)
+            {
+                case 0:
+                    if (chart1.ChartAreas.Count > 2)
+                    {
+                        chart1.ChartAreas.Remove(chart1.ChartAreas.FindByName("Area2"));
+                        chart1.ChartAreas.Remove(chart1.ChartAreas.FindByName("Area3"));
+                        chart1.Series[1].ChartArea = chart1.ChartAreas[0].Name;
+                        chart1.Series[2].ChartArea = chart1.ChartAreas[0].Name;
+                    }
+                    chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+                    chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+                    chart1.Series[2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+                    break;
+                case 1:
+                    if (chart1.ChartAreas.Count > 2)
+                    {
+                        chart1.ChartAreas.Remove(chart1.ChartAreas.FindByName("Area2"));
+                        chart1.ChartAreas.Remove(chart1.ChartAreas.FindByName("Area3"));
+                        chart1.Series[1].ChartArea = chart1.ChartAreas[0].Name;
+                        chart1.Series[2].ChartArea = chart1.ChartAreas[0].Name;
+                    }
+                    chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
+                    chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
+                    chart1.Series[2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
+                    break;
+                case 2:
+                    chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+
+                    chart1.ChartAreas.Add(Area2);
+                    chart1.Series[1].ChartArea = "Area2";
+                    chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+
+                    chart1.ChartAreas.Add(Area3);
+                    chart1.Series[2].ChartArea = "Area3";
+                    chart1.Series[2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+
+                    break;
+                case 3:
+                    if (chart1.ChartAreas.Count > 2)
+                    {
+                        chart1.ChartAreas.Remove(chart1.ChartAreas.FindByName("Area2"));
+                        chart1.ChartAreas.Remove(chart1.ChartAreas.FindByName("Area3"));
+                        chart1.Series[1].ChartArea = chart1.ChartAreas[0].Name;
+                        chart1.Series[2].ChartArea = chart1.ChartAreas[0].Name;
+                    }
+
+                    chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                    chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                    chart1.Series[2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                    break;
+                case 4:
+                    if (chart1.ChartAreas.Count > 2)
+                    {
+                        chart1.ChartAreas.Remove(chart1.ChartAreas.FindByName("Area2"));
+                        chart1.ChartAreas.Remove(chart1.ChartAreas.FindByName("Area3"));
+                        chart1.Series[1].ChartArea = chart1.ChartAreas[0].Name;
+                        chart1.Series[2].ChartArea = chart1.ChartAreas[0].Name;
+                    }
+                    chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+                    chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+                    chart1.Series[2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+                    break;
+                default:
+                    chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+                    chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+                    chart1.Series[2].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+                    break;
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnColorSerie_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                chart1.Series[0].Color = colorDialog1.Color;
+            }
+        }
+
+        private void btnSerie2_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                chart1.Series[1].Color = colorDialog1.Color;
+            }
+        }
+
+        private void btnSerie3_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                chart1.Series[2].Color = colorDialog1.Color;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cbIluminacion.SelectedItem)
+            {
+                case "Ninguno":
+                    chart1.ChartAreas[0].Area3DStyle.LightStyle = LightStyle.None;
+                    break;
+                case "Simplista":
+                    chart1.ChartAreas[0].Area3DStyle.LightStyle = LightStyle.Simplistic;
+                    break;
+                case "Realista":
+                    chart1.ChartAreas[0].Area3DStyle.LightStyle = LightStyle.Realistic;
+                    break;
+                default:
+                    chart1.ChartAreas[0].Area3DStyle.LightStyle = LightStyle.Simplistic;
+                    break;
+            }
+        }
+
+        private void btnBackColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                chart1.ChartAreas[0].BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void cbSeries_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            switch (cbSeries.SelectedIndex)
+            {
+                case 0:
+                    chart1.Series[0].Enabled = true;
+                    chart1.Series[1].Enabled = false;
+                    chart1.Series[2].Enabled = false;
+                    break;
+                case 1:
+                    chart1.Series[0].Enabled = false;
+                    chart1.Series[1].Enabled = true;
+                    chart1.Series[2].Enabled = false;
+                    break;
+                case 2:
+                    chart1.Series[0].Enabled = false;
+                    chart1.Series[1].Enabled = false;
+                    chart1.Series[2].Enabled = true;
+                    break;
+                default:
+                    chart1.Series[0].Enabled = true;
+                    chart1.Series[1].Enabled = false;
+                    chart1.Series[2].Enabled = false;
+                    break;
+            }
+        }
+
+        private void btnTodas_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Enabled = true;
+            chart1.Series[1].Enabled = true;
+            chart1.Series[2].Enabled = true;
+        }
+
+
+    }
+}
